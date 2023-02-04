21baca0f4253b791a63a32c30f5ad3a226170dc6
Felipe Weiss <felipe.weiss@proway.treina>
2023-02-03 21:40:06 -0300

    Crud Student

---
+++ controller/students.py
@@ -0,0 +1,45 @@
+from database import engine
+from sqlmodel import Session, select
+from models.model import  Student
+from sqlalchemy.orm import selectinload
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
+
+
+def findStudent():
+    with Session(engine) as session:
+        statement = select(Student)
+        results = session.exec(statement).all()
+        print(results)
+
+def createStudent(idClassRoom):
+    with Session(engine) as session:
+        new_class = Student(id=None, name=input("Nome do aluno: "), contact=input("Contato do aluno: "), id_classroom=idClassRoom)
+        session.add(new_class)
+        session.commit()
+        #session.refresh(new_class)
+        print(new_class)
+
+def editStudent(studentID, dadosAluno: Student):
+    with Session(engine) as session:
+        statement = select(Student).where(Student.id == studentID)
+        results = session.exec(statement).first()
+        results.name = dadosAluno.name
+        results.contact = dadosAluno.contact
+
+        session.add(results)
+        session.commit()
+        session.refresh(results)
+        print(results)
+        return JSONResponse(content=jsonable_encoder(results))
+
+def deleteStudent(studentID):
+    with Session(engine) as session:
+        statement = select(Student).where(Student.id == studentID)
+        results = session.exec(statement).first()
+        session.delete(results)
+        session.commit()
+        
+        return print('Deleted Student')
+
+
